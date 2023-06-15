003fddf2ddd347217b33af135672a0fae3a25cfe
Ketsebaot-Ertumo <125948844+Ketsebaot-Ertumo@users.noreply.github.com>
2023-06-15 09:41:35 -0700

    commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -23,7 +23,7 @@ exports.createPost = async(req,res, next) =>{
                 url: result.secure_url
             },
 
-          //upload video  
+    //upload video  
 
     }];
     res.status(201),json({
@@ -43,7 +43,7 @@ exports.createPost = async(req,res, next) =>{
 //show post
 exports.showPost = async (req, res,next) =>{
     try{
-        const posts = await Post.field().port({createdit: -1}).populated('postedBy','name');
+        const posts = await Post.field().port({createdAt: -1}).populated('postedBy','name');
         res.status(201).json({
             success: true,
             posts
@@ -57,7 +57,7 @@ exports.showPost = async (req, res,next) =>{
 //show single post
 exports.showSinglePost = async (req, res,next) =>{
     try{
-        const posts = await Post.fieldById(req.user._id).port({createdit: -1}).populated('postedBy','name');
+        const posts = await Post.fieldById(req.paramsid).populated('comments.postedBy', 'name');
         res.status(201).json({
             success: true,
             posts
@@ -68,8 +68,120 @@ exports.showSinglePost = async (req, res,next) =>{
 }
 }
 
+
 //delete post
+exports.deletePost = async (req, res,next) =>{
+    const currentPost = await Post.fieldById(req.params,id);
+    
+    //delete the post image in cloudinary
+    const ImgId = currentPost.image.public_id;
+    if(ImgId){
+        await cloudinary.Uploader.destroy(ImgId);
+    }
+    try{
+        const Post = await Post.fieldByIdAndRemove(req.params.id);
+        res.status(200).json({
+            success: true,
+            message:"post delete"
+        })
+    }
+ catch (error){
+    next(error);
+}
+}
 
 //update post
+exports.updatePost = async (req, res,next) =>{
+    try{
+        const {title, content, image, videos} = req.body;
+        const currentPost = await Post.findById(req.params.id);
+      
+        //build the object data
+        const dafa = {
+            title: title || currentPost.title,
+            content: content || currentPost.content,
+            image: image || currentPost.image,
+        }
 
-//
+        //modify post image conditionally
+        if(req.body.image !== ' ' ){
+            const ImgId = currentPost.image.public_id;
+            if (ImgId) {
+                await cloudinary.Uploader.destroy(ImgId);
+            }
+
+            const newImage = await cloudinary.Uploader.upload(req.body.image, {
+                 folder: 'posts', width: 1200, crop: "scale"
+            });
+
+            data.image = {
+                public_id: newImage.public_id,
+                url: newImage.secure_url
+            }
+        }
+            const postUpdate = await Post.findByIdAndUpdate(req.params.id, date, {new: true});
+            res.status(200).json({
+                success: true,
+                postUpdate
+            })
+    } catch(error){
+        next(error);
+    }
+        }
+
+//comment
+exports.addComment = async (req, res,next) =>{
+    const {comment} = req.body;
+    try{
+        const post = await Post.fieldByIdAndUpdate(req.params.id, {
+            $push: {comments: {text: comment, postedBy: req.user._id}}
+        },
+            {new: true}
+        )
+        res.status(201).json({
+            success: true,
+            post
+        })
+    }  catch (error){
+        next(error);
+    }
+}
+
+
+//add like
+exports.addLike = async (req, res,next) =>{
+    try{
+        const post = await Post.fieldByIdAndUpdate(req.params.id,{
+            $addToSet: {likes: req.user._id}
+        },
+            {new: true}
+        ) 
+        res.status(200).json({
+            success: true,
+            post
+        })
+    } catch (error){
+        next(error);
+}
+}
+
+
+//remove like
+exports.removeLike = async (req, res,next) =>{
+    try{
+        const post = await Post.fieldByIdAndUpdate(req.params.id,{
+            $pull: {likes: req.user._id}
+        },
+            {new: true}
+        ) 
+        res.status(200).json({
+            success: true,
+            post
+        })
+    } catch (error){
+        next(error);
+}
+}
+
+
+
